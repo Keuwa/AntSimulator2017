afc5104edcd2e7dd1b68f02cf4751093d6228a68
Guillaume Chieb Bouares <guillaume@horsecom.io>
2017-06-26 12:28:03 +0200

    add some GUI

---
+++ Assets/Script/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	public void exitGame() {
+		Application.Quit();
+	}
+
+	public void changeScene(string sceneName) {
+		Application.LoadLevel(sceneName);
+	}
+}
